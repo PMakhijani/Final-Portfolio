8fb7ab37a349e9359ba1ce41691971469ea2280c
Piyush Makhijani <24110259@iitgn.ac.in>
2025-07-16 13:32:35 -0300

    Update ParallaxBackground.jsx overlay 50

--- src/Components/ParallaxBackground.jsx
+++ src/Components/ParallaxBackground.jsx
@@ -9,7 +9,7 @@ const ParallaxBackground = () => {
     const mountain2Y = useTransform(x,[0,0.5], ["0%", "30%"]);
     const mountain1Y = useTransform(x,[0,0.5], ["0%", "0%"]);
   return (
-    <section className="absolute inset-0 bg-black/60 ">
+    <section className="absolute inset-0 bg-black/50 ">
         <div className="relative h-screen overflow-y-hidden">
             {/*Background video*/}
         <video
